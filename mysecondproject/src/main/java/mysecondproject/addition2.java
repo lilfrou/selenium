6f3a5f96cad6c3d8c6396124eaa9308a25b48aef
lilfrou <51007021+lilfrou@users.noreply.github.com>
2019-08-08 16:24:22 +0100

    Add files via upload

---
+++ mysecondproject/src/main/java/mysecondproject/addition2.java
@@ -0,0 +1,10 @@
+package mysecondproject;
+
+public class addition2 {
+	public static int add2( int operand1, int operand2 ) {
+		 return operand1 + operand2;
+		 }
+	public static int div2( int operand1 , int operand2 ) {
+		return operand1 / operand2;
+		}
+}
---
+++ mysecondproject/src/test/java/mysecondproject/addition2test2.java
@@ -0,0 +1,21 @@
+package mysecondproject;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+
+
+public class addition2test2 {
+	@Test
+	public void testAdd() {
+	AssertJUnit.assertEquals(3, addition2.add2(1, 2));
+	System.out.println("testadd");
+	}
+	@Test
+	public void testDiv() {
+		 AssertJUnit.assertEquals(1, addition2.div2(3, 2));
+		 System.out.println("testdiv");
+		 }
+}
