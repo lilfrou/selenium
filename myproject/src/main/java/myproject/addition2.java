75996d0478a362526b8e84630f4bc42b0d5edb27
lilfrou <51007021+lilfrou@users.noreply.github.com>
2019-08-14 16:56:07 +0100

    Add files via upload

---
+++ myproject/src/main/java/myproject/addition2.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class addition2 {
+	public static int add( int operand1, int operand2 ) {
+		 return operand1 + operand2;
+		 }
+	public static int div( int operand1 , int operand2 ) {
+		return operand1 / operand2;
+		}
+
+}
